051d53e834bd9fd406f4b0fda3d6b77d1bf6c1fd
Даниил Яндыбаев <xramirezx@outlook.com>
2020-09-13 04:57:02 +0400

    feat: add solutions for 06

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,8 +20,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 
@@ -35,8 +41,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +57,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.assign(Object.create(proto), JSON.parse(json));
 }
 
 
@@ -110,33 +116,102 @@ function fromJSON(/* proto, json */) {
  *  For more examples see unit tests.
  */
 
+
+class StyleSelectorBuilder {
+  constructor() {
+    this.result = '';
+    this.hasElem = false;
+    this.hasId = false;
+    this.hasPseudoElem = false;
+    this.priority = 6;
+  }
+
+  checkPriorityOrder(priority) {
+    if (priority > this.priority) throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    this.priority = priority;
+  }
+
+  element(value) {
+    this.checkPriorityOrder(6);
+    if (this.hasElem) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    this.result += value;
+    this.hasElem = true;
+    return this;
+  }
+
+  id(value) {
+    this.checkPriorityOrder(5);
+    if (this.hasId) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    this.result += `#${value}`;
+    this.hasId = true;
+    return this;
+  }
+
+  class(value) {
+    this.checkPriorityOrder(4);
+    this.result += `.${value}`;
+    return this;
+  }
+
+  attr(value) {
+    this.checkPriorityOrder(3);
+    this.result += `[${value}]`;
+    return this;
+  }
+
+  pseudoClass(value) {
+    this.checkPriorityOrder(2);
+    this.result += `:${value}`;
+    return this;
+  }
+
+  pseudoElement(value) {
+    this.checkPriorityOrder(1);
+    if (this.hasPseudoElem) throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    this.result += `::${value}`;
+    this.hasPseudoElem = true;
+    return this;
+  }
+
+  combine(selector1, combinator, selector2) {
+    this.result = `${selector1.stringify()} ${combinator} ${selector2.stringify()}`;
+    return this;
+  }
+
+  stringify() {
+    return this.result;
+  }
+}
+
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  result: '',
+
+  element(value) {
+    return new StyleSelectorBuilder().element(value);
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    return new StyleSelectorBuilder().id(value);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return new StyleSelectorBuilder().class(value);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return new StyleSelectorBuilder().attr(value);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return new StyleSelectorBuilder().pseudoClass(value);
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return new StyleSelectorBuilder().pseudoElement(value);
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  combine(selector1, combinator, selector2) {
+    return new StyleSelectorBuilder().combine(selector1, combinator, selector2);
   },
 };
 
